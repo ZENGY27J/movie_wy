c75f64ff293cfc334adf6373787b283e7e900b45
ZENGY27J <zexyzm@163.com>
2019-10-13 11:51:12 +0800

    @zy

--- movie_wy/wy-api/src/main/java/com/wuyan/user/UserService.java
+++ movie_wy/wy-api/src/main/java/com/wuyan/user/UserService.java
@@ -1,6 +1,52 @@
 package com.wuyan.user;
 
+import com.wuyan.user.bean.UserInfoModel;
+import com.wuyan.user.bean.UserModel;
+
 public interface UserService {
 
     String getNameById(Integer id);
+
+    /**
+     * 用户注册接口
+     * @param userModel
+     * @return
+     */
+    boolean register(UserModel userModel);
+
+    /**
+     * 用户名验证接口
+     * @param username
+     * @return
+     */
+    boolean checkUserName(String username);
+
+    /**
+     * 用户登录接口
+     * @param username
+     * @param password
+     * @return 返回id表示登录成功，返回null表示登录失败
+     */
+    Integer login(String username, String password);
+
+    /**
+     * 用户退出接口
+     * @param id
+     * @return
+     */
+    int logout(Integer id);
+
+    /**
+     * 用户信息查询接口
+     * @param id
+     * @return
+     */
+    UserInfoModel queryUserInfo(Integer id);
+
+    /**
+     * 修改用户信息的接口
+     * @param userInfoModel
+     * @return
+     */
+    UserInfoModel updateUserInfo(UserInfoModel userInfoModel);
 }
--- movie_wy/wy-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
+++ movie_wy/wy-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserController.java
@@ -1,7 +1,10 @@
 package com.stylefeng.guns.rest.modular.user;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.stylefeng.guns.rest.modular.vo.ReBaseMsgVo;
+import com.stylefeng.guns.rest.modular.vo.ReBaseVo;
 import com.wuyan.user.UserService;
+import com.wuyan.user.bean.UserModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2019-10-12-20:29
  */
 @RestController
+@RequestMapping("user")
 public class UserController {
 
     @Reference(interfaceClass = UserService.class)
@@ -23,4 +27,49 @@ public class UserController {
         return name;
     }
 
+    /**
+     * 用户登录
+     * @param userModel
+     * @return
+     */
+    @RequestMapping("/register")
+    public ReBaseVo register(UserModel userModel) {
+        boolean register = false;
+        try {
+            register = userService.register(userModel);
+            if (register) {
+                ReBaseMsgVo ok = ReBaseMsgVo.ok();
+                ok.setMsg("注册成功");
+                return ok;
+            }
+            ReBaseMsgVo serviceEx = ReBaseMsgVo.serviceEx();
+            serviceEx.setMsg("用户已存在");
+            return serviceEx;
+        } catch (Exception e) {
+            return ReBaseMsgVo.systemEx();
+        }
+    }
+
+    /**
+     * 用户名验证
+     * @param username
+     * @return
+     */
+    @RequestMapping("/check")
+    public ReBaseVo check(String username) {
+        boolean checkUserName = false;
+        try {
+            checkUserName = userService.checkUserName(username);
+            if (checkUserName) {
+                ReBaseMsgVo ok = ReBaseMsgVo.ok();
+                ok.setMsg("用户名不存在");
+                return ok;
+            }
+            ReBaseMsgVo serviceEx = ReBaseMsgVo.serviceEx();
+            serviceEx.setMsg("用户已注册");
+            return serviceEx;
+        } catch (Exception e) {
+            return ReBaseMsgVo.systemEx();
+        }
+    }
 }
